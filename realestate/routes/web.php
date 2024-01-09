42850aba0ea157ca1795489c71ad6565c4ac6586
ronak <chavadaronak007@gmail.com>
2024-01-09 10:53:55 +0530

    ok

--- realestate/routes/web.php
+++ realestate/routes/web.php
@@ -2,8 +2,6 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\AgentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,5 +29,3 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
-Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
-Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
