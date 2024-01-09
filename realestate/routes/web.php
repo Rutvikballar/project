f4aa4be13aaa1205a104b4582a293b73d4ebac09
chauhan vivek <125378382+vivek7669@users.noreply.github.com>
2024-01-09 20:45:41 +0530

    vivek
    i am completed video no 20.
    😀

--- realestate/routes/web.php
+++ realestate/routes/web.php
@@ -34,6 +34,7 @@ require __DIR__.'/auth.php';
 
 Route::middleware(['auth','role:admin'])->group(function(){
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
 
 });//End Group Admin Middleware
 
@@ -41,3 +42,6 @@ Route::middleware(['auth','role:agent'])->group(function(){
 
 Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
 });//End Group Agent Middleware
+
+Route::get('/admin/logout', [AgentController::class, 'AdminLogout'])->name('admin.logout');
+});
