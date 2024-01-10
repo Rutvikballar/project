dffcf1639823125b96221e584a23d8f5c0b816b2
Jemish-Rathod <jemishrathod2004@gmail.com>
2024-01-10 15:10:30 +0530

    video 27 is completed

--- realestate/app/Http/Controllers/AdminController.php
+++ realestate/app/Http/Controllers/AdminController.php
@@ -5,14 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\user;
+
 class AdminController extends Controller
 {
     public function AdminDashboard()
     {
         return view('admin.index');
-    }//End Method
+    } //End Method
 
-    public function AdminLogout(Request $request){
+    public function AdminLogout(Request $request)
+    {
         Auth::guard('web')->logout();
 
         $request->session()->invalidate();
@@ -20,18 +22,40 @@ class AdminController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('/admin/login');
-    }//End Method
+    } //End Method
 
     public function AdminLogin()
     {
         return view('admin.admin_login');
-    }//End Method
+    } //End Method
 
     public function AdminProfile()
     {
         $id = Auth::user()->id;
         $profileData = User::find($id);
-        return view('admin.admin_profile_view',compact('profileData'));
-    }//End Method
-}
+        return view('admin.admin_profile_view', compact('profileData'));
+    } //End Method
 
+    public function AdminProfileStore(Request $request)
+    {
+
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->username = $request->username;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'), $filename);
+            $data['photo'] = $filename;
+        }
+
+        $data->save();
+        return redirect()->back();
+    } //End Method
+
+}
